d6cfd96a2f1f00f3567ca6e9d1c792d2792197e6
estampher <114256515+estampher@users.noreply.github.com>
2022-11-28 22:31:59 -0800

    Add files via upload
    Removes tweets about Ukraine conflict

---
+++ ukraine_remove.py
@@ -0,0 +1,14 @@
+#%% REMOVING TWEETS ABOUT UKRAINE CONFLICT
+
+import pandas as pd
+
+data = pd.read_csv(r'*.csv',header=None)
+
+#%%
+dataremoved = data[~data.iloc[:,1].str.contains('Ukraine')]
+dataremoved = dataremoved[~data.iloc[:,1].str.contains('Ukrainian')]
+dataremoved = dataremoved[~data.iloc[:,1].str.contains('Ukrainians')]
+dataremoved = dataremoved[~data.iloc[:,1].str.contains('Zaporizhzhia')]
+
+dataremoved.to_csv('remainder.csv',encoding='utf-8-sig', header=None)
+
