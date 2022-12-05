8d7771cbf562cc30ea2ed6dbf1a47dec98193946
estampher <114256515+estampher@users.noreply.github.com>
2022-12-05 02:43:05 -0800

    updated list of filter keywords

--- ukraine_remove.py
+++ ukraine_remove.py
@@ -10,8 +10,18 @@ tweet_df = tweet_df[['tweet','link']]
 tweet_df = tweet_df.drop_duplicates()
 
 #%%
-dataremoved = tweet_df[~tweet_df.iloc[:,1].str.contains('Ukraine')]
-dataremoved = dataremoved[~tweet_df.iloc[:,1].str.contains('Ukrainian')]
-dataremoved = dataremoved[~tweet_df.iloc[:,1].str.contains('Ukrainians')]
-dataremoved = dataremoved[~tweet_df.iloc[:,1].str.contains('Zaporizhzhia')]
+dataremoved = data[~data.iloc[:,0].str.contains('Ukraine')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('ukraine')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Ukrainian')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('ukrainian')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Ukrainians')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('ukrainians')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Zaporizhzhia')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('war')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('russia')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Russia')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('russian')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Russian')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('russians')]
+dataremoved = dataremoved[~data.iloc[:,0].str.contains('Russians')]
 # %%
