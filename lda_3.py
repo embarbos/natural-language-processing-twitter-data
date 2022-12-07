cf5ce487677e502b2cb1f9dfec29392be7c258fb
estampher <114256515+estampher@users.noreply.github.com>
2022-12-06 19:09:09 -0800

    Update lda_3.py

--- lda_3.py
+++ lda_3.py
@@ -1,20 +1,20 @@
 #%%
-import json
 import pandas as pd
 import nltk
 
-#%% This is taking in the labelled.csv file and formatting it for the rest of the code
-
-data = pd.read_csv('labelled.csv',header=None)
-data = data.iloc[:,[1,2]]
-data.columns = ['tweets','label']
+#%%
+#all.csv is in the github, it is all of the tweets including the training set tweets we worked on, with the ukraine/russian/war tweets filtered out
+data = pd.read_csv('all.csv',header=None)
+#%%
+data = data.iloc[:,1]
+data.columns = ['tweets']
 
 # %% NLP PRE-PROCESSING
 import re
 
 cleaned_tweets = []
 words = []
-for tweet in data['tweets']:
+for tweet in data:
     clean = re.sub(r"(http[s]?\://\S+)|([\[\(].*[\)\]])|([#@]\S+)|\n", " ", tweet)
     clean = re.sub(r"\d", '', clean)
     clean = re.sub(r"'\S+", '', clean)
@@ -36,33 +36,44 @@ corpus_lemmatized = [[nltk.WordNetLemmatizer().lemmatize(word) for word in docum
 #stitching back together
 corpus = [' '.join(document) for document in corpus_lemmatized]
 
+
 #%% GENSIM LDA
+
 import numpy as np
 import re
 import string
+#import spacy
 import gensim
 from gensim import corpora
+#import pyLDAvis
+#import pyLDAvis.gensim_models
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#%% the gensim model needs this kind of encoding it seems
+#%% encoding
 dictionary = corpora.Dictionary(corpus_lemmatized)
-doc_term_matrix = [dictionary.doc2bow(rev) for rev in corpus_lemmatized] # this produces tuples with (wordID, frequency)
+doc_term_matrix = [dictionary.doc2bow(rev) for rev in corpus_lemmatized]
 
-#%% LDA MODEL
+#%%
+# Creating the object for LDA model using gensim library
 LDA = gensim.models.ldamodel.LdaModel
-lda_model = LDA(corpus=doc_term_matrix, id2word=dictionary, num_topics=10, random_state=100,
+
+# Build LDA model (2 topic)
+lda_model = LDA(corpus=doc_term_matrix, id2word=dictionary, num_topics=2, random_state=100,
                 chunksize=1000, passes=50,iterations=100)
 
-#%% topic words
-print(lda_model.print_topics())
+#%% topic display
+posterior = lda_model.print_topics()
+two_topic_LDA = pd.DataFrame(posterior)[1]
+two_topic_LDA = two_topic_LDA.transpose()
+two_topic_LDA.index = ['topic ' + str(i) for i in range(0,2)]
+two_topic_LDA.name = 'words'
 
-#%% per tweet based topic proportions example (for tweet #2 here-- needs to be looped)
-vector = lda_model.get_document_topics(doc_term_matrix[1], minimum_probability=0, minimum_phi_value=None, per_word_topics=False)
+print(two_topic_LDA)
 
-#%% Perplexity and coherence scores for the current LDA model k value
+#%% Perplexity and coherence scores (u_mass coherence scores)
+
 print('\nPerplexity: ', lda_model.log_perplexity(doc_term_matrix,total_docs=10000))  # a measure of how good the model is. lower the better.
-
 # Compute Coherence Score
 from gensim.models.coherencemodel import CoherenceModel
 coherence_model_lda = CoherenceModel(model=lda_model, texts=corpus_lemmatized, dictionary=dictionary , coherence='u_mass')
@@ -71,7 +82,9 @@ if __name__ == "__main__":
     coherence_lda = coherence_model_lda.get_coherence()
     print('\nCoherence Score: ', coherence_lda)
 
-#%% SETTING UP TO GRAPH COHERENCE SCORES BY K VALUE
+
+#%% GRAPH FUNCTION
+
 def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
     """
     Compute c_v coherence for various number of topics
@@ -98,31 +111,47 @@ def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
 
     return model_list, coherence_values
 
-#%% uhhh then run this
+#%% list of coherence scores for different topic numbers
+
 model_list, coherence_values = compute_coherence_values(dictionary=dictionary, corpus=doc_term_matrix, texts=corpus_lemmatized, start=2, limit=50, step=1)
 
-#%% AND HERE'S THE GRAPH
+#%%
+# Show graph
 limit=50; start=2; step=1;
 x = range(start, limit, step)
 plt.plot(x, coherence_values)
 plt.xlabel("Num Topics")
 plt.ylabel("Coherence score")
 plt.legend(("coherence_values"), loc='best')
-plt.show()
+plt.show()# Print the coherence scores
 
-#%% Print the coherence scores
+#%%
+# Print the coherence scores
 for m, cv in zip(x, coherence_values):
     print("Num Topics =", m, " has Coherence Value of", round(cv, 4))
 
+#%% the interactive visualization
 
-
-
-#%% the interaction visualization
 import pyLDAvis
 import pyLDAvis.gensim_models
 vis_data = pyLDAvis.gensim_models.prepare(lda_model, doc_term_matrix, dictionary)
 pyLDAvis.show(vis_data, open_browser=False, local=False)
 
+#%% MATRIX TIME, THIS OUTPUT WILL GO INTO K-NN
+
+tweet_vectors = pd.Series(0)
+for i in range(len(doc_term_matrix)):    
+    tweet_vectors[i] = lda_model.get_document_topics(doc_term_matrix[i], minimum_probability=0, minimum_phi_value=None, per_word_topics=False)
+
+#%%
+
+tweet_vectors_entries = [[tweet_vectors[i][0][1],tweet_vectors[i][1][1]] for i in range(len(tweet_vectors))]
+
+LDA_tweet_frame = pd.DataFrame(tweet_vectors_entries, columns = ['Topic 0','Topic 1'])
+
+#%%
+
+LDA_tweet_frame.to_csv('LDA_tweet_vectors.csv',header=None)
 
 
 
